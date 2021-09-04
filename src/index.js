3bc37d094d59661f7e2f6f952afdd981035152b2
SugarPlum321 <87677021+SugarPlum321@users.noreply.github.com>
2021-09-03 23:48:37 -0400

    Add more Info
    Added
    wind speed, weather text.
    set up icons

--- src/index.js
+++ src/index.js
@@ -19,7 +19,6 @@ dateIsNow.innerHTML = `${displayDate()}`;
 function seachCity(city) {
   let apiKey = "a05ae6cedba9f2ae2d858f2bc163bc51";
   let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
-  console.log(apiURL);
   axios.get(apiURL).then(getInfo);
 }
 
@@ -35,15 +34,23 @@ formCity.addEventListener("submit", displayCity);
 
 //feching Today infor
 function getInfo(response) {
+  console.log(response.data);
   document.querySelector("#city-Location").innerHTML = response.data.name;
   let tempF = Math.round(response.data.main.temp);
   let todayTempFah = document.querySelector("#todayFahrenheit");
   todayTempFah.innerHTML = `${tempF}°F`;
+  let tempC = Math.round(((response.data.main.temp - 32) * 5) / 9);
+  let todayTempCel = document.querySelector("#todayCelsius");
+  todayTempCel.innerHTML = `${tempC}°C`;
   let humidity = response.data.main.humidity;
   let todayhumidity = document.querySelector("#humidity");
   todayhumidity.innerHTML = `${humidity}`;
-  let tempC = Math.round(((response.data.main.temp - 32) * 5) / 9);
-
-  let todayTempCel = document.querySelector("#todayCelsius");
-  todayTempCel.innerHTML = `${tempC}°C`;
+  document.querySelector("#weatherDiscrip").innerHTML =
+    response.data.weather[0].description;
+  document.querySelector("#windSpeed").innerHTML = response.data.wind.speed;
+  //icon
+  document.querySelector("#todayIcon").setAttribute =
+    response.data.weather[0].icon;
 }
+
+//and weather icon are mandatory.
