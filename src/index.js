5542b99be016f3070c0541c095477761e2c2d1df
SugarPlum321 <87677021+SugarPlum321@users.noreply.github.com>
2021-09-10 22:20:59 -0400

    add the 5 day forecast
    also fix some css line up

--- src/index.js
+++ src/index.js
@@ -47,7 +47,9 @@ function getInfo(response) {
   todayhumidity.innerHTML = `${humidity}`;
   document.querySelector("#weatherDiscrip").innerHTML =
     response.data.weather[0].description;
-  document.querySelector("#windSpeed").innerHTML = response.data.wind.speed;
+  document.querySelector("#windSpeed").innerHTML = Math.round(
+    response.data.wind.speed
+  );
   //icon
   let todayIcon = response.data.weather[0].icon;
   document
@@ -59,39 +61,63 @@ function getInfo(response) {
   //5 day forcast call
   getForecast(response.data.coord);
 }
-//5 day forcast
+
+//5 day forcast information call
 function getForecast(coordinates) {
   let apiKey = "a05ae6cedba9f2ae2d858f2bc163bc51";
   let apiURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=imperial`;
   console.log(apiURL);
   axios.get(apiURL).then(displayForecast);
 }
-
+//formate the 5 day forcast
+function formateDays(timeStamp) {
+  let date = new Date(timeStamp * 1000);
+  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tusday",
+    "Wensday",
+    "Thursaday",
+    "Friday",
+    "Saturday",
+  ];
+  return days[day];
+}
+//5day forcast display
 function displayForecast(response) {
+  let forecast = response.data.daily;
   console.log(response.data.daily);
   let forecastDays = document.querySelector("#forcast-Days");
   let forecastHTML = "";
-  let days = ["Monday", "Tuseday", "Wensday"];
-  days.forEach(function (day) {
+
+  forecast.forEach(function (forecastFive, index) {
     //display box
-    forecastHTML =
-      forecastHTML +
-      `
+    if (index < 5) {
+      forecastHTML =
+        forecastHTML +
+        `
   <div class="dayBox" class="col-12" >
-    <h4 class="day">${day}</h4>
+    <h4 class="day">${formateDays(forecastFive.dt)}</h4>
       <div class="row">
         <div class="col">
-        <img class="fiveDayIcon" src="img/icon_clear sky.png" />
+        <img class="fiveDayIcon" src="http://openweathermap.org/img/wn/${
+          forecastFive.weather[0].icon
+        }@2x.png"/>
+         
         </div>
         <div class="col">
-        <h3 class="fahrenheit">93°F</h3>
+        <h3 class="fahrenheit"> ${Math.round(forecastFive.temp.day)}°F</h3>
           <div class="row">
-          <h3 class="celsius">34°c</h3>
+          <h3 class="celsius">${Math.round(
+            ((forecastFive.temp.day - 32) * 5) / 9
+          )}°C</h3>
           </div>
         </div>
       </div>
     </div>
   `;
+    }
   });
 
   forecastDays.innerHTML = forecastHTML;
